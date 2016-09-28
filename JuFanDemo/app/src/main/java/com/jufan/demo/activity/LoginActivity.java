9b7ebafc9061438c2061089f064fef32edc78e8d
TomorrowSunshine <m13994887936@163.com>
2016-09-28 10:29:38 +0800

    登陆

--- JuFanDemo/app/src/main/java/com/jufan/demo/activity/LoginActivity.java
+++ JuFanDemo/app/src/main/java/com/jufan/demo/activity/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.jufan.demo.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.jufan.demo.R;
 
@@ -13,19 +15,24 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        //找到控件
-//        ImageView qq=(ImageView) findViewById(R.id.qq);
-//        ImageView weiyin=(ImageView) findViewById(R.id.weixin);
-//        ImageView phone=(ImageView) findViewById(R.id.phone);
-//        ImageView weibo=(ImageView) findViewById(R.id.weibo);
+        //找到控件
+        ImageView qq=(ImageView) findViewById(R.id.qq);
+        ImageView weixin=(ImageView) findViewById(R.id.weixin);
+        ImageView phone=(ImageView) findViewById(R.id.phone);
+        ImageView weibo=(ImageView) findViewById(R.id.weibo);
+
+        weixin.setOnClickListener(this);
     }
     //点击登陆事件
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.qq:
+
                 break;
             case R.id.weixin:
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                finish();
                 break;
             case R.id.phone:
                 break;
